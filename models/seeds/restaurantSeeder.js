fcd350351ac2155e9ca6678711704c9561bb7889
lingnili <wmdlifecoba@gmail.com>
2019-11-25 00:12:39 +0800

    Upload seeder to A14 homework assignment

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -3,6 +3,7 @@ const Restaurant = require("../restaurant");
 const restaurantSample = require("./restaurant.json");
 const userSample = require("./users.json");
 const User = require("../../models/user");
+const bcrypt = require("bcrypt");
 
 mongoose.connect("mongodb://localhost/restaurant", {
   useNewUrlParser: true,
@@ -20,27 +21,40 @@ db.once("open", () => {
   console.log("mongodb connected!");
   const data = restaurantSample.results;
   const user = userSample.results;
+
   //從json新增種子資料到restaurant model
-  const newUser = new User({
-    name: "test",
-    email: user[0].email,
-    password: user[0].password
-  });
-  newUser.save().then(user => {
-    for (let i = 0; i < data.length; i++) {
-      Restaurant.create({
-        name: data[i].name,
-        rating: data[i].rating,
-        category: data[i].category,
-        image: data[i].image,
-        location: data[i].location,
-        phone: data[i].phone,
-        description: data[i].description,
-        //建立user跟restaurant關聯後，需把userId丟到restaurant document中
-        userId: user._id
+  for (let j = 0; j < user.length; j++) {
+    //password須經bcrypt
+    const newUser = new User({
+      name: user[j].name,
+      email: user[j].email,
+      password: user[j].password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        newUser.password = hash;
+
+        newUser.save().then(user => {
+          for (let i = 3 * j; i <= 3 * j + 2; i++) {
+            Restaurant.create({
+              name: data[i].name,
+              rating: data[i].rating,
+              category: data[i].category,
+              image: data[i].image,
+              location: data[i].location,
+              phone: data[i].phone,
+              description: data[i].description,
+              //建立user跟restaurant關聯後，需把userId丟到restaurant document中
+              userId: user._id
+            });
+          }
+        });
       });
-    }
-  });
+    });
+
+    console.log("test");
+  }
 
   console.log("done");
 });
